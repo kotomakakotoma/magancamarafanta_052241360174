b2a5cafb7527aa3937a0e2ed95f3f8b55bc975a0
kotomaka <kotomakakotoma@gmail.com>
2024-03-12 13:29:29 +0000

    ok

---
+++ studentdushbord.php
@@ -0,0 +1,32 @@
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>admin</title>
+   <link rel="stylesheet"   type="text/css" href="admin.css">
+
+</head>
+<body>
+<header class="her">
+   <a href="">Student Dushbord</a>
+   <div class="logout">
+      <a href="login.php">logout</a>
+   </div>
+</header>
+<aside>
+   <ul>
+      <li>
+       <a href="">My courses</a>
+      </li>
+      <li>
+       <a href="">My results</a>
+      </li>
+     
+   </ul>
+</aside>
+
+</body>
+</html>
